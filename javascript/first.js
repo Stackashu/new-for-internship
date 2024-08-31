c2b458b0e82f5e921f407a55da5039b4de2d18bf
Stackashu <ashu12141214@gmail.com>
2024-08-31 07:48:18 +0530

    added first js file

---
+++ javascript/first.js
@@ -0,0 +1,17 @@
+console.log("ash")
+let a = "ash"
+console.log(a)
+
+a=44
+console.log(a)
+
+const b = 34
+console.log(b)
+
+// b=443
+console.log(b)
+
+var c = 43
+console.log(c)
+ c = 49
+console.log(c)
